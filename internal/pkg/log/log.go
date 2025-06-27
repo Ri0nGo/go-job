697f18c9ed829daef74bf650b27e862c4466add9
Ri0nGO <920728039@qq.com>
2025-06-27 16:38:24 +0800

    支持日志存储到本地

--- internal/pkg/log/log.go
+++ internal/pkg/log/log.go
@@ -1,12 +1,22 @@
 package log
 
 import (
+	"github.com/natefinch/lumberjack"
 	"log/slog"
 	"os"
 )
 
 func InitSlog(level slog.Level, source bool) {
-	logger := slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+	_ = os.MkdirAll("./logs", 0755)
+
+	writer := &lumberjack.Logger{
+		Filename:   "./logs/app.log", // 日志文件路径
+		MaxSize:    100,              // 单个日志文件最大100MB
+		MaxBackups: 10,               // 最多保留5个旧文件
+		MaxAge:     30,               // 日志保留30天
+		Compress:   true,             // 是否压缩归档日志
+	}
+	logger := slog.New(slog.NewTextHandler(writer, &slog.HandlerOptions{
 		Level:     level,
 		AddSource: source,
 	}))
