79a546d336edf72efb0231695d1d1502e6d41300
Ri0nGO <920728039@qq.com>
2025-09-02 18:50:46 +0800

    Fix: node token 未刷新问题

--- node/pkg/auth/token.go
+++ node/pkg/auth/token.go
@@ -4,6 +4,7 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 	"go-job/internal/model"
 	"go-job/internal/pkg/auth"
+	"sync"
 	"time"
 )
 
@@ -14,46 +15,44 @@ const (
 	defaultUserId           = -1
 )
 
-//var uc = model.UserClaims{
-//	RegisteredClaims: jwt.RegisteredClaims{
-//		ExpiresAt: jwt.NewNumericDate(time.Now().Add(defaultExpireTime)),
-//	},
-//	Uid: defaultUserId,
-//}
-
-var uc = model.UserClaims{
-	RegisteredClaims: jwt.RegisteredClaims{
-		ExpiresAt: jwt.NewNumericDate(time.Now().Add(defaultJwtExpireTime)),
-	},
-	Uid: defaultUserId,
-}
-
 type JwtToken struct {
 	builder     *auth.JwtBuilder // TODO 这里可以改成接口，不同的校验都去实现生成和校验接口，这样就热插拔的方式了
 	token       string
 	ExpireTime  time.Time     `json:"expire_time"`  // 过期时间
 	RefreshTime time.Duration `json:"refresh_time"` // 过期前多久刷新
+	mux         sync.RWMutex
 }
 
 var jt = &JwtToken{}
 
 func InitJwtToken(key string) {
 	jt.builder = auth.NewJwtBuilder(key)
+	jt.RefreshTime = defaultRefreshTimeLimit
 }
 
 func RefreshToken() error {
+	jt.mux.Lock()
+	defer jt.mux.Unlock()
 	// 初始化token， 快过期刷新token
 	if jt.token == "" || jt.ExpireTime.Sub(time.Now()) < jt.RefreshTime {
+		jt.ExpireTime = time.Now().Add(defaultExpireTime)
+		uc := model.UserClaims{
+			RegisteredClaims: jwt.RegisteredClaims{
+				ExpiresAt: jwt.NewNumericDate(jt.ExpireTime),
+			},
+			Uid: defaultUserId,
+		}
 		token, err := jt.builder.GenerateToken(uc)
 		if err != nil {
 			return err
 		}
 		jt.token = token
-
 	}
 	return nil
 }
 
 func GetJwtToken() string {
+	jt.mux.RLock()
+	defer jt.mux.RUnlock()
 	return jt.token
 }
